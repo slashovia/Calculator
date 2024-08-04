358c9da8f8362012ab92204b7414d5435ca17fce
Hashmi <digiacomo93@gmail.com>
2024-08-04 20:49:09 +0200

    Start from 0, and finish the project

--- script.js
+++ script.js
@@ -1,40 +1,83 @@
-const display = document.querySelector('.display');
-const backspace = document.querySelector('.backspace');
-const add = document.querySelector('.add');
-const subtract = document.querySelector('.subtract');
-const multiply = document.querySelector('.multiply');
-const divide = document.querySelector('.divide');
-const equal = document.querySelector('.equal');
-const dot = document.querySelector('.dot');
-const zero = document.querySelector('.zero');
-const one = document.querySelector('.one');
-const two = document.querySelector('.two');
-const three = document.querySelector('.three');
-const four = document.querySelector('.four');
-const five = document.querySelector('.five');
-const six = document.querySelector('.six');
-const seven = document.querySelector('.seven');
-const height = document.querySelector('.height');
-const nine = document.querySelector('.nine');
+let display = document.querySelector('.display');
+let clear = document.querySelector('.clear');
+let numbers = document.querySelectorAll('.number');
+let operators = document.querySelectorAll('.operator');
+let equal = document.querySelector('.equal');
+let decimal = document.querySelector('.decimal');
 
-let variable1;
-let variable2;
-let operator;
-let result;
+let previousNumber = '';
+let currentNumber = '';
+let currentOperator = '';
 
-function operate(num1, num2, operator) {
-    switch (operator) {
-        case divide.addEventListener('click'):
-            return result = num1 / num2;
-            break;
-        case multiply.addEventListener('click'):
-            return result = num1 * num2;
-            break;
-        case add.addEventListener('click'):
-            return result = num1 + num2;
-            break;
-        case subtract.addEventListener('click'):
-            return result = num1 - num2;
-            break;
+numbers.forEach((number) => number.addEventListener('click', (e) => {
+    getNumber(e.target.textContent)
+    display.textContent = currentNumber;
+}));
+
+
+operators.forEach((operator) => operator.addEventListener('click', (e) => {
+    getOperator(e.target.textContent)
+    display.textContent = currentOperator;
+
+}))
+
+equal.addEventListener('click', () => {
+    if (currentNumber != '' && previousNumber != '') {
+        operate();
+        if (previousNumber.length <= 7) {
+            display.textContent = previousNumber;
+        }
+        else {
+            display.textContent = previousNumber.slice(0, 7) + '...';
+        }
+    }
+})
+
+clear.addEventListener('click', () => {
+    currentNumber = '';
+    previousNumber = '';
+    currentOperator = '';
+    display.textContent = currentNumber;
+})
+
+decimal.addEventListener('click', () => {
+    getDecimal();
+    display.textContent = currentNumber;
+})
+function getOperator(operator) {
+    currentOperator = operator;
+    previousNumber = currentNumber;
+    currentNumber = '';
+}
+
+function getNumber(num) {
+    if (currentNumber.length < 7) {
+        currentNumber += num;
+    }
+}
+
+function operate() {
+    previousNumber = Number(previousNumber);
+    currentNumber = Number(currentNumber);
+    if (currentOperator == '+') {
+        previousNumber += currentNumber;
+    }
+    else if (currentOperator == '-') {
+        previousNumber -= currentNumber;
+    }
+    else if (currentOperator == 'x') {
+        previousNumber *= currentNumber;
+    }
+    else {
+        previousNumber /= currentNumber;
+    }
+    previousNumber = previousNumber.toString();
+    currentNumber = currentNumber.toString();
+}
+
+
+function getDecimal() {
+    if (!currentNumber.includes('.')) {
+        currentNumber += '.';
     }
 }
